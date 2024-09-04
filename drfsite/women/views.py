0fdd00ac71a958570351401c873080f7009f252f
YanaZamiralova <yana.zam@icloud.com>
2024-09-04 13:57:09 +0300

    Представление generic-views

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,20 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Women
+from .serializers import WomenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class WomenAPIList(generics. ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
